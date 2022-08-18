49f2d93ab8f2976a8e9dee0b38fdbd349f0a0197
Thayssa Bernardo <thayssabernardo@gmail.com>
2022-08-18 20:44:22 -0300

    Deletando arquivos

--- app.js
+++ app.js
@@ -14,12 +14,4 @@ app.use('/sales', saleRoute);
 
 app.use(Error.err);
 
-// não remova esse endpoint, é para o avaliador funcionar
-app.get('/', (_request, response) => {
-  response.send();
-});
-
-// não remova essa exportação, é para o avaliador funcionar
-// você pode registrar suas rotas normalmente, como o exemplo acima
-// você deve usar o arquivo index.js para executar sua aplicação 
 module.exports = app;
